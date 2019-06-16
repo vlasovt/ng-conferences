2161459a82de078f8b4d9814509b96c2cb5a34e2
vlasovt <tarasvlasov@gmail.com>
2019-06-15 21:17:23 -0400

    added templated login form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { FormsModule } from '@angular/forms';
 import {userRoutes} from './user.routes';
+import { LoginComponent } from './login/login.component';
  
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent, LoginComponent],
   imports: [
     CommonModule, 
+    FormsModule,
     RouterModule.forChild(userRoutes)
   ],
   providers: [
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,8 @@
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const userRoutes = [
-    {path: 'profile', component: ProfileComponent}
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent }
 ]
